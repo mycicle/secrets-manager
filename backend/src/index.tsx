ab7f18f1c2fc40faff18e3b2ece6a24ee812d43d
Mycicle <mjm.digregorio@gmail.com>
2021-03-01 16:56:22 -0500

    Organized Backend Folder
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- backend/src/index.tsx
+++ backend/src/index.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 import express, { Express } from "express";
 import cors from "cors";
 import connectMongo from './connectdb';
@@ -17,19 +14,4 @@ connectMongo(() =>
     app.listen(PORT, () => 
         console.log(`Server running on http://localhost:${PORT}`)
     )
-);
-
-const Content = () => (
-  <h1>My React and TypeScript App! {" "}
-  {new Date().toLocaleDateString()}</h1>
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Content />
-    </Router>
-  </React.StrictMode>,
-
-  document.getElementById("root")
 );
